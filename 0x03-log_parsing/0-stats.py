de81e15ed319ec78c6b4985f4ad6129ee56d2772
yonil1-hub <wyml@yahoo.com>
2023-04-13 19:34:11 +0000

    refactor the entire code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
+
 """
-This script parses a log file and prints the
-file size and status code metrics.
+Script that reads standard input line by line and computes metrics
 """
 
 import sys
 
 
-def print_metrics(file_size, status_codes):
+def print_status(code_dict, total_size):
     """
-    This function prints the file size and status code metrics.
-    Args:
-        file_size (int): Total file size in bytes.
-        status_codes (dict): A dictionary containing counts of
-        each HTTP status code.
+    Prints the computed metrics.
+    :param code_dict: a dictionary containing status codes
+    as keys and their count as values
+    :param total_size: total size of the processed file
     """
-    print("File size: {}".format(file_size))
-    codes_sorted = sorted(status_codes.keys())
-    for code in codes_sorted:
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    print("File size: {:d}".format(total_size))
+    for code in sorted(code_dict.keys()):
+        if code_dict[code] != 0:
+            print("{}: {:d}".format(code, code_dict[code]))
 
 
-if __name__ == "__main__":
-    codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
-                   '403': 0, '404': 0, '405': 0, '500': 0}
-    file_size_total = 0
-    count = 0
+# Dictionary to store count of status codes
+status_codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+        "404": 0, "405": 0, "500": 0
+        }
 
-    try:
-        for line in sys.stdin:
-            try:
-                status_code = line.split()[-2]
-                if status_code in codes_count:
-                    codes_count[status_code] += 1
+count = 0
+total_size = 0
 
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
+try:
+    for line in sys.stdin:
+        if count % 10 == 0 and count != 0:
+            print_status(status_codes, total_size)
 
-            except Exception:
-                pass
+        items = line.split()
+        count += 1
 
-            # Print metrics every 10 lines.
-            count += 1
-            if count == 10:
-                print_metrics(file_size_total, codes_count)
-                count = 0
+        try:
+            total_size += int(items[-1])
+        except ValueError:
+            pass
 
-    except KeyboardInterrupt:
-        print_metrics(file_size_total, codes_count)
-        raise
+        try:
+            if items[-2] in status_codes:
+                status_codes[items[-2]] += 1
+        except IndexError:
+            pass
 
-    print_metrics(file_size_total, codes_count)
+    print_status(status_codes, total_size)
 
+except KeyboardInterrupt:
+    print_status(status_codes, total_size)
+    raise
