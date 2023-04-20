410e865d17a746da626703715946d9aef093ac10
yonil1-hub <wyml@yahoo.com>
2023-04-20 18:11:03 +0000

    valid utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""file: 0-validate_utf8.py"""
+
+
+def validUTF8(data):
+    """Determines if a given data set represents
+    a valid UTF-8 encoding or not.
+    """
+    num_bytes = 0
+    for byte in data:
+        if num_bytes == 0:
+            if (byte >> 5) == 0b110:
+                num_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                num_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                num_bytes = 3
+            elif (byte >> 7) != 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
