67bbd33d25f56404accfa4e77f20638560b067a8
yonil1-hub <wyml@yahoo.com>
2023-06-02 11:53:05 +0300

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,61 +1,54 @@
 #!/usr/bin/python3
 
 """
-This module implements a game where Maria and Ben take turns
-choosing prime numbers from a set of consecutive integers.
-The player that cannot make a move loses the game.
-The module provides a function to determine the
-winner of each game.
-
-
+This module implements a game where Maria and Ben take turns choosing prime
+numbers from a set of consecutive integers. The player that cannot make a
+move loses the game. Themodule provides a function to
+determine the winner of each game.
 """
 
 
 def isWinner(x, nums):
     """
-    Determines the winner of each game based on the given number of
-    rounds and sets of consecutive integers.
-
+    Determines the winner of each game based on the given number of rounds
     Args:
         x (int): The number of rounds.
-        nums (list): An array of integers representing the sets of
-        consecutive integers.
+        nums (list): An array of integers
 
     Returns:
         str: The name of the player that won the most rounds.
-        If the winner cannot be determined, returns None.
+        If the winner cannot be
+        determined, returns None.
 
     """
+
+    def is_prime(num):
+        """
+        Checks if a number is prime.
+
+        Args:
+            num (int): The number to check.
+
+        Returns:
+            bool: True if the number is prime, False otherwise.
+
+        """
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        numbers = set(range(1, n + 1))
-        player = "Maria"
-
-        while True:
-            prime = None
-            for num in numbers:
-                if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)):
-                    prime = num
-                    break
-
-            if prime is None:
-                break
-
-            for num in range(prime, n + 1, prime):
-                numbers.discard(num)
-
-            if player == "Maria":
-                player = "Ben"
-            else:
-                player = "Maria"
-
-        if player == "Maria":
-            maria_wins += 1
-        else:
+        prime_count = sum(is_prime(i) for i in range(1, n + 1))
+        if prime_count % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
         return "Maria"
