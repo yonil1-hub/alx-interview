64ba61723fb18fb67edcb0a40a1f0440b24f27ee
Jamescog <jamescog72@gmail.com>
2023-06-10 13:24:11 +0300

    better implementation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,87 +1,52 @@
 #!/usr/bin/python3
-"""Log parsing"""
+"""
+Log parser - computes metrics from log files.
+"""
+
+from sys import stdin
 
 
-import re
+def print_statistics(total_size, status_counts):
+    """Prints the computed statistics"""
+    print(f"Total file size: {total_size}")
+    for code, count in sorted(status_counts.items()):
+        if count > 0:
+            print(f"{code}: {count}")
 
 
-def parse_log_line(line):
-    """
-    Parses a line of an HTTP request log and extracts relevant information.
-
-    Args:
-        line (str): The line of the HTTP request log.
-
-    Returns:
-        dict: A dictionary containing the extracted information.
-            Keys: 'ip', 'date', 'request', 'status_code', 'file_size'.
-    """
-    log_regex = (
-        r'\s*(?P<ip>\d+\.\d+\.\d+\.\d+)\s*-\s*\[(?P<date>[^\]]+)\]\s*'
-        r'"GET /projects/260 HTTP/1.1"\s*(?P<status_code>\d+)\s*(?P<file_size>\d+)'
-    )
-    match = re.match(log_regex, line)
-    if match:
-        return match.groupdict()
-    else:
-        return {}
-
-
-def print_statistics(total_file_size, status_code_counts):
-    """
-    Prints the accumulated statistics of the HTTP request log.
-
-    Args:
-        total_file_size (int): The total file size.
-        status_code_counts (dict): A dictionary containing the counts of each status code.
-            Keys: status codes, Values: count of occurrences.
-    """
-    print(f"File size: {total_file_size}")
-    for status_code, count in sorted(status_code_counts.items()):
-        print(f"{status_code}: {count}")
-
-
-def update_metrics(line, total_file_size, status_code_counts):
-    """
-    Updates the metrics based on a line of the HTTP request log.
-
-    Args:
-        line (str): The line of the HTTP request log.
-        total_file_size (int): The current total file size.
-        status_code_counts (dict): A dictionary containing the counts of each status code.
-            Keys: status codes, Values: count of occurrences.
-
-    Returns:
-        int: The updated total file size.
-    """
-    log_info = parse_log_line(line)
-    if log_info:
-        status_code = log_info['status_code']
-        file_size = int(log_info['file_size'])
-        total_file_size += file_size
-        status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
-    return total_file_size
-
-
-def run_log_parser():
-    """
-    Starts the log parser and computes the statistics.
-    """
-    total_file_size = 0
-    status_code_counts = {}
+def compute_metrics():
+    """Reads log lines from stdin and computes metrics"""
+    status_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    total_size = 0
+    line_count = 0
 
     try:
-        line_num = 0
-        while True:
-            line = input()
-            total_file_size = update_metrics(line, total_file_size, status_code_counts)
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_code_counts)
+        for line in stdin:
+            parts = line.split()
+            if line_count % 10 == 0 and line_count != 0:
+                print_statistics(total_size, status_counts)
 
-    except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_code_counts)
+            try:
+                status_code = parts[-2]
+                if status_code in status_counts:
+                    status_counts[status_code] += 1
+            except IndexError:
+                pass
+
+            try:
+                file_size = int(parts[-1])
+                total_size += file_size
+            except (IndexError, ValueError):
+                pass
+
+            line_count += 1
+
+        print_statistics(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_counts)
+        raise
 
 
 if __name__ == '__main__':
-    run_log_parser()
+    compute_metrics()
